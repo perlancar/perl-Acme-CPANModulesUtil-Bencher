04972d21f8bceafde1aca3df2cbc9990ff1c63d6
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-01-20 13:31:45 +0700

    [ux] Add alias module Bencher::Util::CPANModules

---
+++ lib/Bencher/Util/CPANModules.pm
@@ -0,0 +1,19 @@
+package Bencher::Util::CPANModules;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Alias for Acme::CPANModulesUtil::Bencher
+
+=head1 DESCRIPTION
+
+This is an alias for L<Acme::CPANModulesUtil::Bencher> to help finding the
+module easier.
+
+
+=head1 SEE ALSO
+
+L<Acme::CPANModulesUtil::Bencher>
